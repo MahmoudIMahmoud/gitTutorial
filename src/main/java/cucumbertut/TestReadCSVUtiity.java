57a9be8da429395c6f62266d9075bb64bbe086df
MahmoudIMahmoud <mahmoud.ibrahim1396@gmail.com>
2018-12-26 00:25:12 +0200

    get the 2d array's rows and columns.

--- src/main/java/cucumbertut/TestReadCSVUtiity.java
+++ src/main/java/cucumbertut/TestReadCSVUtiity.java
@@ -6,6 +6,8 @@ public class TestReadCSVUtiity {
 
 	public static void main(String[] args) throws IOException {
 		String[][] data=CSVReaderUtil.csv2array("./data/data.csv", true);
+		System.out.println("row count:"+data.length);
+		System.out.println("col count:"+data[0].length);
 		for(String[] row:data)
 			for(String col:row)
 				System.out.println(col);
